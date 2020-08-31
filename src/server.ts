99f98043c9a0fd5df78e81362ebea6a7d8d155ee
Beto Taborda <marcelotaborda72@gmail.com>
2020-08-31 06:17:19 -0300

    fix var port

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ class Server {
     public constructor() {
         this.express = express();
         this.app = new App(this.express);
-        const PORT = '3000';
+        const PORT = process.env.PORT || '3000';
         this.up(Number(PORT));
         this.app.upServer();
     }
