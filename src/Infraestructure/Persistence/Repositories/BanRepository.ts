c1b5576247e37bf1b8b04e1d74e8f3eeb409a487
santidalmasso <santidalmassoj@gmail.com>
2020-11-13 17:57:26 -0300

    feat: add teams by id route and refactors

--- src/Infraestructure/Persistence/Repositories/BanRepository.ts
+++ src/Infraestructure/Persistence/Repositories/BanRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import TypeRepository from './TypeRepository';
 import { IBanRepository } from '../../../Domain/Contracts/Repositories/IBanRepository';
 import Ban from '../../../Domain/Entities/Ban';
@@ -8,7 +9,11 @@ class BanRepository extends TypeRepository implements IBanRepository {
     }
 
     public async findAll(): Promise<Ban[]> {
-        return await this.repository(Ban).find();
+        return await this.repository(Ban).find({ relations: ['player'] });
+    }
+
+    public async find(option: any): Promise<Ban[]> {
+        return await this.repository(Ban).find(option);
     }
 }
 
--- src/Presentation/Http/Routes/team.ts
+++ src/Presentation/Http/Routes/team.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { asyncMiddleware } from '../Middelwares/AsyncMiddleware';
 import CreateTeamAction from '../Actions/Team/CreateTeamAction';
 import IndexTeamAction from '../Actions/Team/IndexTeamAction';
+import IndexByIdTeamAction from '../Actions/Team/IndexByIdTeamAction';
 import UpdateTeamAction from '../Actions/Team/UpdateTeamAction';
 import DeleteTeamAction from '../Actions/Team/DeleteTeamAction';
 import { inject, injectable } from 'inversify';
@@ -10,24 +11,28 @@ import { isAuthenticatedMiddleware } from '../Middelwares/Auth/IsAuthenticatedMi
 import { CreateTeamSchema } from '../Validations/Schemas/Team/CreateTeamSchema';
 import { UpdateTeamSchema } from '../Validations/Schemas/Team/UpdateTeamSchema';
 import { DeleteTeamSchema } from '../Validations/Schemas/Team/DeleteTeamSchema';
+import { IndexByIdTeamSchema } from '../Validations/Schemas/Team/IndexByIdTeamSchema';
 
 @injectable()
 class TeamRoutes {
     private router: Router;
     private createTeamAction: CreateTeamAction;
     private indexTeamAction: IndexTeamAction;
+    private indexByIdTeamPlayerAction: IndexByIdTeamAction;
     private updateTeamAction: UpdateTeamAction;
     private deleteTeamAction: DeleteTeamAction;
 
     public constructor(
         @inject(CreateTeamAction) createTeamAction: CreateTeamAction,
         @inject(IndexTeamAction) indexTeamAction: IndexTeamAction,
+        @inject(IndexByIdTeamAction) indexByIdTeamAction: IndexByIdTeamAction,
         @inject(UpdateTeamAction) updateTeamAction: UpdateTeamAction,
         @inject(DeleteTeamAction) deleteTeamAction: DeleteTeamAction,
     ) {
         this.router = Router();
         this.createTeamAction = createTeamAction;
         this.indexTeamAction = indexTeamAction;
+        this.indexByIdTeamPlayerAction = indexByIdTeamAction;
         this.updateTeamAction = updateTeamAction;
         this.deleteTeamAction = deleteTeamAction;
         this.setRoutes();
@@ -47,6 +52,15 @@ class TeamRoutes {
             isAuthenticatedMiddleware(),
             asyncMiddleware(async (request: Request, response: Response) => {
                 await this.indexTeamAction.execute(request, response);
+            }),
+        );
+
+        this.router.get(
+            '/:id',
+            isAuthenticatedMiddleware(),
+            validationMiddleware(IndexByIdTeamSchema, 'params'),
+            asyncMiddleware(async (request: Request, response: Response) => {
+                await this.indexByIdTeamPlayerAction.execute(request, response);
             }),
         );
 
